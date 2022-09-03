b47166f6b6a699d60e974e9138869530e793b493
optimisticside <47266822+optimisticside@users.noreply.github.com>
2022-09-03 11:37:05 -0700

    Fix RK4 solver

--- src/flightctrl/src/math.rs
+++ src/flightctrl/src/math.rs
@@ -3,20 +3,20 @@ pub type DiffEq<T: FloatCore> = fn(x: T, y: T) -> T;
 
 /// Implementation of the Range-Kutta 4th Order method of solving differential equations.
 /// Finds the value for y given x and a step size, as well as an initial value (x0, y0).
-pub fn rk4<T: FloatCore>(equation: DiffEq<T>, x0: T, y0: T, x: T, step_size: T) -> T {
+pub fn rk4<T: FloatCore>(equation: DiffEq<T>, mut x0: T, mut y0: T, mut x: T, step_size: T) -> T {
     // Count the number of iterations using the stp size.
     let iter_count = ((x - x0) / step_size) as usize;
-    let kutta[T; 4] = [0 as T; 4];
-    let y = y0;
+    let (mut k1, mut k2, mut k3, mut k4): (T, T, T, T) = (0, 0, 0, 0);
+    let mut y = y0;
 
     for i in 1..iter_count {
-        kutta[0] = step_size * equation(x0, y);
-        kutta[1] = step_size * equation(x0 + (0.5 * step_size), y + (0.5 * kutta[0]));
-        kutta[2] = step_size * equation(x0 + (0.5 * step_size), y + (0.5 * kutta[1]));
-        kutta[3] = step_size * equation(x0 + step_size, y + kutta[2]);
+        k1 = step_size * equation(x0, y);
+        k2 = step_size * equation(x0 + (0.5 * step_size), y + (0.5 * k1));
+        k3 = step_size * equation(x0 + (0.5 * step_size), y + (0.5 * k2));
+        k4 = step_size * equation(x0 + step_size, y + k3);
 
         // Update next value of x and y. Rust should optimize the 1/6 in the beginning.
-        y += ((1 as T) / 6) * (kutta[0] + (2 * kutta[1]) + (2 * kutta[2]) + kutta[3]);
+        y += ((1 as T) / 6) * (k1 + (2 * k2) + (2 * k3) + k4);
         x0 += step_size;
     }
 
